396bfea0fdfb73f1e541bf77a1c834ebbf94eefa
Jonathan <jonathan.jib@hotmail.com>
2022-09-25 17:53:36 -0300

    00001 - Spring Initializer

---
+++ src/main/java/com/ferbal/gestionapp/GestionappApplication.java
@@ -0,0 +1,13 @@
+package com.ferbal.gestionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferbal/gestionapp/GestionappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferbal.gestionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
