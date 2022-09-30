3be96954a70db9e9ffd594a58a1652e9a6ba4c95
Jonathan <jonathan.jib@hotmail.com>
2022-09-30 00:36:53 -0300

    00007 -  Backend Customer Structure
    Getter and Setters

--- src/main/java/com/ferbal/gestionapp/customer/Customer.java
+++ src/main/java/com/ferbal/gestionapp/customer/Customer.java
@@ -1,4 +1,26 @@
 package com.ferbal.gestionapp.customer;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CUSTOMER")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "ID")
+    private int id;
+
+    @Getter @Setter @Column(name = "ID_CUSTOMER")
+    private String idCustomer;
+
+    @Getter @Setter @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Getter @Setter @Column(name = "LAST_NAME")
+    private String lastName;
+
 }
